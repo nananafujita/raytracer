93066a9e62ed20debc9456c0217437f214194367
Nana Fujita <nanaefuj@usc.edu>
2025-05-04 03:32:05 +0900

    implemented basic glfw logic, window creation, and keycallback

--- src/main.c
+++ src/main.c
@@ -4,14 +4,51 @@
 
 #include "scene.h"
 
+const int WIDTH = 800;
+const int HEIGHT = 600;
+
+void key_callback(GLFWwindow* wind, int key, int scancode, int action, int mods) 
+{
+    switch(key)
+    {
+        case(256):
+            exit(0);
+            break;
+    }
+} 
+
 int main(int argc, char* argv[]) 
 {
     if (!glfwInit()) {
-        printf("Failed to initialize GLFW. Exiting program.\n");
+        fprintf(stderr, "Failed to initialize GLFW. Exiting program.\n");
         return -1;
     }
 
     if (load_scene(argv[1]) != 0) {
         return -1;
     }
+
+    GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "Ray Tracer", NULL, NULL);
+    if (!window) {
+        fprintf(stderr, "Failed to create GLFW window. Exiting program.\n");
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glfwSetKeyCallback(window, key_callback);
+
+    //init();
+
+    while (!glfwWindowShouldClose(window)) {
+       //display(); 
+        //idle();    
+
+        glfwSwapBuffers(window);
+        glfwPollEvents(); 
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
 }
